c573958fff97c518f55171f8b118d748f8dd59fd
Malik Amir <115186951+malkimir-1999@users.noreply.github.com>
2024-08-06 01:14:46 +0500

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleMapsComponent } from './shared/google-maps/google-maps.component';
 import { PaymentStatusComponent } from './shared/payment-status/payment-status.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './platform/main-dashboard/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // HomeComponent,
     GoogleMapsComponent,
     PaymentStatusComponent,
-
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
--- src/app/platform/main-dashboard/home/home.component.ts
+++ src/app/platform/main-dashboard/home/home.component.ts
@@ -7,17 +7,26 @@ import { LinksComponent } from 'src/app/shared/links/links.component';
 import { TestimonialComponent } from 'src/app/platform/main-dashboard/testimonial/testimonial.component';
 import { WorkflowComponent } from '../workflow/workflow.component';
 import { KeyFeaturesComponent } from '../key-featuers/key-features.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, LinksComponent, TestimonialComponent, WorkflowComponent, KeyFeaturesComponent]
+  imports: [CommonModule, FormsModule, RouterModule, HeaderComponent, FooterComponent, LinksComponent, TestimonialComponent, WorkflowComponent, KeyFeaturesComponent]
 })
 export class HomeComponent {
 
+  selectedSpeciality: string = '';
+  selectedService: string = '';
+  selectedLocation: string = '';
+  selectedDate: string = '';
 
-
-  
+  search() {
+    console.log('Speciality:', this.selectedSpeciality);
+    console.log('Service:', this.selectedService);
+    console.log('Location:', this.selectedLocation);
+    console.log('Date:', this.selectedDate);
+  }
 }
