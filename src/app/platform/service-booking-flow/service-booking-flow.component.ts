437fe0b3b731918b50579c774103940625d28816
Usman Bin Saad <usmanbinsaad01@gmail.com>
2024-08-09 00:18:01 +0500

    all changes implemented

--- src/app/platform/service-booking-flow/service-booking-flow.component.ts
+++ src/app/platform/service-booking-flow/service-booking-flow.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { first } from 'rxjs';
+import { authenticationService } from 'src/app/services/authentication.service';
+import { Global } from '../../utilities/Global';
+import { homeService } from 'src/app/services/home.service';
+
 
 @Component({
   selector: 'app-service-booking-flow',
-  standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent,],
   templateUrl: './service-booking-flow.component.html',
-  styleUrls: ['./service-booking-flow.component.scss']
+  styleUrls: ['./service-booking-flow.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, FormsModule, ReactiveFormsModule],
 })
-export class ServiceBookingFlowComponent {
+export class ServiceBookingFlowComponent implements OnInit {
+
+
+  personalInformationStepper: boolean = true;
+  practiceInformationStepper: boolean = false;
+  skillsInformationStepper: boolean = false;
+  accountInformationStepper: boolean = false;
+  successfulStepper: boolean = false;
+
+  constructor(public formBuilder: FormBuilder) {
+
+  }
+
+
+  ngOnInit() {
+
+  }
 
 }
---
+++ src/app/utilities/Global.ts
@@ -0,0 +1,8 @@
+
+export class Global {
+    emailPattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    mobilePattern = '\d{8,10}$';
+    numericPattern = '^[0-9]*$';
+    namePattern = '^[^!@#$%^`"":;~&\*()+-.?><{}\/\\|؟0-9]{1,50}$';
+    // passwordPattern: "^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$";
+} 
